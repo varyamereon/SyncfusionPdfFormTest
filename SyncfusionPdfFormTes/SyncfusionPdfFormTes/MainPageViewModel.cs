4937a299a215873f24bfe8cb3ff22f67cea77784
Will Beal <willbeal@outlook.com>
2021-08-31 19:30:59 +0200

    Add project files.

---
+++ SyncfusionPdfFormTes/SyncfusionPdfFormTes/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MvvmHelpers;
+using Syncfusion.Pdf.Parsing;
+using System.IO;
+using System.Reflection;
+
+namespace SyncfusionPdfFormTest
+{
+	public class MainPageViewModel : BaseViewModel
+	{
+		private Stream pdfStream;
+		public Stream PdfStream
+		{
+			get => pdfStream;
+			set => SetProperty(ref pdfStream, value);
+		}
+
+		public MainPageViewModel()
+		{
+			using var pdfFile = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("SyncfusionPdfFormTest.Resources.TestPdf.pdf");
+			var document = new PdfLoadedDocument(pdfFile);
+
+			var form = document.Form;
+			form.SetDefaultAppearance(false);
+
+			if (form.Fields["totalHours"] is PdfLoadedTextBoxField totalField)
+			{
+				totalField.Text = "I changed it!";
+			}
+
+			pdfStream = new MemoryStream();
+			document.Save(pdfStream);
+		}
+	}
+}
